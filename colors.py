7b9b4da6c9dac604f81dd3d4db0193b7f8fec713
Connor Shorten <connorshorten300@gmail.com>
2022-10-14 20:51:31 -0400

    Update colors.py

--- colors.py
+++ colors.py
@@ -9,6 +9,27 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 # colors = f.readlines()
 # that would take you right to the for color in colors:
 
+'''
+{
+  "color": "Red",
+  "rgb_vector": (255,255,0),
+  "SentenceTransformer": {
+    "model_name": "all-MiniLM-L6-v2",
+    "vector": [0.3, 0.5, ..., 1.6],
+    "UMAP-3D-vector": [1.7, 2.7, 8.1],
+    "UMAP-2D-vector": [8.5, 11.7],
+   },
+   "SentenceTransformer": {
+    "model_name": "paraphrase-MiniLM-L6-v2",
+    "vector": [1.4, 0.02, ..., 0.08],
+    "UMAP-3D-vector": [1.708, 9.7, 8.9],
+    "UMAP-2D-vector": [10.5, 0.7],
+   }
+  ...
+}
+'''
+
+
 colors = [
     {
         "color": "red",
