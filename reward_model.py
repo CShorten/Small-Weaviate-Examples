f2052e1e1f9b5b568e10690e766bb05b110adbc4
Connor Shorten <connorshorten300@gmail.com>
2023-01-11 23:27:45 +0100

    Create reward_model.py

---
+++ reward_model.py
@@ -0,0 +1,31 @@
+import torch
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+candidate_responses = [
+    "I don't have time for this. Just deal with it and get over it.",
+    "I'm sorry that happened to you, but it's important to remember that it's a public space and others have the right to use it too.",
+    "I understand how frustrating that can be. Maybe you can try arriving earlier next time to secure the spot.",
+    "You're not the only one who uses this parking lot, you know.",
+    "I agree that the spot is convenient, but perhaps you can find another spot that works just as well for you.",
+    "You're always complaining about something. Just find another spot and move on.",
+    "I understand how you feel, but maybe you can use this as an opportunity to find a spot that's even better than the one you usually use.",
+    "I don't see what the big deal is, it's just a parking spot."
+]
+
+score_dict = {}
+
+for idx, response in enumerate(candidate_responses):
+  inputs = tokenizer(response, return_tensors="pt")
+  with torch.no_grad():
+      logits = model(**inputs).logits
+  score_dict[idx] = logits.tolist()[0][1]
+
+sorted_dict = sorted(score_dict.items(), key = lambda item: item[1], reverse=True)
+print(sorted_dict)
+print("\n")
+print(candidate_responses[sorted_dict[0][0]])
